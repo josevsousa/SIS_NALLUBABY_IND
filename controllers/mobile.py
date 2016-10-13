d3477954735f3ec650e8de114f2ab19dd0cd21c7
python <josev@josev.com.br>
2016-10-13 12:08:00 -0300

    mobile 20%

--- controllers/mobile.py
+++ controllers/mobile.py
@@ -21,11 +21,11 @@ def produto_destaque():
     produto = db(db.product.id == request.args(0)).select()[0]
     return dict(produto=produto)
 
-def login():
-    # logo da table empresa.
-    logo = IMG(_src=URL('static','images/logo/logo_m.png'), _class="home_img_logo" ,_width='150px')
-    # registro.elements('input')[1].attributes['_class'] = "form-control"
-    return dict(login=auth.login(),logo=logo)
+# def login():
+#     # logo da table empresa.
+#     logo = IMG(_src=URL('static','images/logo/logo_m.png'), _class="home_img_logo" ,_width='150px')
+#     # registro.elements('input')[1].attributes['_class'] = "form-control"
+#     return dict(login=auth.login(),logo=logo)
 
 def dialogo():
     return dict(msg="teste")
